bb6f1c2facc0b11201d2db76c12043e0ab4d6b1e
rdulong <remi.dulong@unine.ch>
2021-02-02 12:06:09 +0000

    add list script to get cluster state of each Pi

---
+++ server/list-pi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NFSPATH='/pxe/nfs'
+TYPES="chaumont chasseral"
+
+for type in $TYPES
+do
+    echo -e "\n=== $type ===\n\n" 
+    for i in $(ls -v $NFSPATH/ | grep $type)
+    do
+	if [ -f $NFSPATH/$i/iiun-cluster-owner ]
+	then
+	    echo "$i : $(cat $NFSPATH/$i/iiun-cluster-owner)"
+	else
+	    echo "$i : FREE"
+	fi
+    done
+    echo -e "\n\n"
+done
+
